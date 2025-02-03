6d0b0cd09b231249494211997d4d02a0d7226677
nasirsaikh <nasirsaikh@gmail.com>
2025-02-03 10:28:01 +0400

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def dashboard():
                          kpis=kpis,
                          data=df.to_dict('records'))
 
-app.run(host='127.0.0.1',port=8000,debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
